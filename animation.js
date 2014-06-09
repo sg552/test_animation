e760509573c9e4761d077696fd6f29b982d654bf
Siwei <sg552sg552@gmail.com>
2014-06-09 15:54:08 +0800

    首页实现了点击菜单，重新播放动画

--- animation.js
+++ animation.js
@@ -1,6 +1,10 @@
 
 // 首页的动画函数
 start_index_animation = function(){
+  if($('#index_animation_is_showing').length > 0) {
+    return;
+  }
+  $('body').append('<div id="index_animation_is_showing"></div>')
   $('.index_wrapper .middle').animate({ opacity: 1.0 , left: "+=10", top: "+=100" }, 1000)
   $('.index_wrapper .left').animate({ opacity: 1.0 , left: "+=500"}, 2000)
   $('.index_wrapper .right').animate({ opacity: 1.0 , left: "-=600"}, 2000, function(){
@@ -8,38 +12,89 @@ start_index_animation = function(){
       $('#text2').animate({opacity: 1.0}, 500, function(){
         $('#text3').animate({opacity: 1.0}, 500, function(){
           $('#text4').animate({opacity: 1.0}, 500, function(){
-            $('#text5').animate({opacity: 1.0}, 500)
+            $('#text5').animate({opacity: 1.0}, 500,function(){
+              $('body').append('<div id="index_animation_is_shown"></div>')
+              $("#index_animation_is_showing").remove()
+            })
           })
         })
       })
     })
   });
-  $('body').append('<div id="index_animation_is_shown"></div>')
 }
+
+cancel_index_animation = function(){
+  if($('#index_animation_is_showing').length > 0) {
+    return;
+  }
+  $('.index_wrapper .middle').css({ opacity: 0 , left: "-=10", top: "-=100" })
+  $('.index_wrapper .left').css({ opacity: 0 , left: "-=500"})
+  $('.index_wrapper .right').css({ opacity: 0 , left: "+=600"})
+  $([1,2,3,4,5]).each(function(i, e){
+    $('#text' + i).css({opacity: 0})
+  })
+  $("#index_animation_is_shown").remove()
+}
+
 
 // 联系我们的动画函数
 start_about_us_animation = function(){
-  $('#about_us_left').animate({ opacity: 1.0, left: '+=500' }, 1000)
-  $('#about_us_right').animate({ opacity: 1.0, left: '-=300' }, 1000)
-  $('body').append('<div id="about_us_animation_is_shown"></div>')
+  if($('#about_us_animation_is_showing').length > 0) {
+    return;
+  }
+  $('body').append('<div id="about_us_animation_is_showing"></div>')
+  $('#about_us_left').animate({ opacity: 1.0, left: '+=500' }, 1000, function(){
+    $('#about_us_right').animate({ opacity: 1.0, left: '-=300' }, 1000, function(){
+      $('body').append('<div id="about_us_animation_is_shown"></div>')
+      $("#about_us_animation_is_showing").remove()
+    })
+  })
+}
+cancel_about_us_animation = function(){
+  if($('#about_us_animation_is_showing').length > 0) {
+    return;
+  }
+  $('#about_us_left').css({ opacity: 0, left: '-=500' })
+  $('#about_us_right').css({ opacity: 0, left: '+=300' })
+  $("#about_us_animation_is_shown").remove()
 }
 
 
 //服务的动画函数
 start_services_animation = function(){
+  if($('#services_animation_is_showing').length > 0) {
+    return;
+  }
+  $('body').append('<div id="services_animation_is_showing"></div>')
   $('#upper_left').animate({ opacity: 1.0, left: "+=400" }, 540, function(){
     $('#upper_left_arrow').animate({ opacity: 1.0})
     $('#upper_right').animate({ opacity: 1.0, left: "-=500" }, 540, function(){
       $('#upper_right_arrow').animate({ opacity: 1.0})
       $('#lower_left').animate({ opacity: 1.0, left: "+=420" }, 540, function(){
         $('#lower_right').animate({ opacity: 1.0, left: "-=470" }, 540, function(){
-          $('#lower_arrows').animate({ opacity: 1.0})
+          $('#lower_arrows').animate({ opacity: 1.0}, 540, function(){
+            $('body').append('<div id="services_animation_is_shown"></div>')
+            $("#services_animation_is_showing").remove()
+          })
         })
       })
     })
   })
   $('.middle').animate({ opacity: 1.0 }, 1000)
-  $('body').append('<div id="services_animation_is_shown"></div>')
+}
+cancel_services_animation = function(){
+  if($('#services_animation_is_showing').length > 0) {
+    return;
+  }
+  $("#services_animation_is_shown").remove()
+  $('#upper_left').css({ opacity: 0, left: "-=400" })
+  $('#upper_left_arrow').css({ opacity: 0})
+  $('#upper_right').css({ opacity: 0, left: "+=500" })
+  $('#upper_right_arrow').css({ opacity: 0})
+  $('#lower_left').css({ opacity: 0, left: "-=420" })
+  $('#lower_right').css({ opacity: 0, left: "+=470" })
+  $('#lower_arrows').css({ opacity: 0})
+  $('.middle').css({ opacity: 0 })
 }
 
 
@@ -67,7 +122,10 @@ show_balls = function(){
             $('#ball_plus_3').animate({ opacity: 1.0, 'margin-left': "-=200"}, 800, function(){
               $('#ball_4').animate({ opacity: 1.0, 'margin-left': "-=200"}, 800, function(){
                 $('#view_button').animate({opacity: 1.0, top: '-=50'}, 1500)
-                $('#balls').animate({ width: 700}, 100)
+                $('#balls').animate({ width: 700}, 100, function(){
+                  $('body').append('<div id="cases_animation_is_shown"></div>')
+                  $("#cases_animation_is_showing").remove()
+                })
               })
             })
           })
@@ -79,13 +137,39 @@ show_balls = function(){
 
 //案例展示的动画函数
 start_cases_animation = function() {
+  if($('#cases_animation_is_showing').length > 0) {
+    return;
+  }
+  $('body').append('<div id="cases_animation_is_showing"></div>')
   $('#text').animate({opacity: 1.0, top: "+=50"}, 1500, function(){
     show_trees()
     $('#sub_title').animate({opacity: 1.0, top: "+=50"}, 1500, function(){
       show_balls()
     })
   })
-  $('body').append('<div id="cases_animation_is_shown"></div>')
+}
+
+cancel_cases_animation = function(){
+  if($('#cases_animation_is_showing').length > 0) {
+    return;
+  }
+  $('#tree1').css({ opacity: 0, left: "-=550"})
+  $('#tree2').css({ opacity: 0})
+  $('#tree3').css({ opacity: 0})
+  $('#tree4').css({ opacity: 0})
+  $('#tree5').css({ opacity: 0})
+  $('#ball_1').css({ opacity: 0, 'margin-left': "+=200"})
+  $('#ball_plus_1').css({ opacity: 0, 'margin-left': "+=200"})
+  $('#ball_2').css({ opacity: 0, 'margin-left': "+=200"})
+  $('#ball_plus_2').css({ opacity: 0, 'margin-left': "+=200"})
+  $('#ball_3').css({ opacity: 0, 'margin-left': "+=200"})
+  $('#ball_plus_3').css({ opacity: 0, 'margin-left': "+=200"})
+  $('#ball_4').css({ opacity: 0, 'margin-left': "+=200"})
+  $('#view_button').css({opacity: 0, top: '+=50'})
+  $('#balls').css({ width: 2000})
+  $('#text').css({opacity: 0, top: "-=50"})
+  $('#sub_title').css({opacity: 0, top: "-=50"})
+  $("#cases_animation_is_shown").remove()
 }
 
 // 开发流程的动画函数
@@ -146,3 +230,37 @@ $(function(){
   $('.top').animate({ opacity: 1.0 }, 1000)
   $('.title').animate({ opacity: 1.0, 'margin-top': '+=50' }, 1000)
 })
+
+
+
+restart_services_animation = function(){
+  if($('#services_animation_is_showing').length > 0 || $('#services_animation_is_shown').length < 1) {
+    return;
+  }
+  cancel_services_animation()
+  start_services_animation()
+}
+
+restart_cases_animation = function(){
+  if($('#cases_animation_is_showing').length > 0 || $('#cases_animation_is_shown').length < 1) {
+    return;
+  }
+  cancel_cases_animation()
+  start_cases_animation()
+}
+
+restart_about_us_animation = function(){
+  if($('#about_us_animation_is_showing').length > 0 || $('#about_us_animation_is_shown').length < 1) {
+    return;
+  }
+  cancel_about_us_animation()
+  start_about_us_animation()
+}
+restart_index_animation = function(){
+  if($('#index_animation_is_showing').length > 0 || $('#index_animation_is_shown').length < 1) {
+    return;
+  }
+
+  cancel_index_animation()
+  start_index_animation()
+}
